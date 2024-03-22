68ee23239be4b8d652043ba78873afd399606ac7
Michelle <nyamburamichelle400@gmail.com>
2024-03-22 16:22:07 +0300

    completes lab

---
+++ index.js
@@ -0,0 +1,25 @@
+function receivesAFunction(callback) {
+    console.log(callback())
+}
+
+
+const returnsANamedFunction =(fn=returnsANamedFunction) => {
+          return returnsANamedFunction
+
+}
+
+
+const returnsAnAnonymousFunction =(fn=returnsAnAnonymousFunction) => {
+       return (function() {
+
+       })
+    }
+
+    
+
+  
+
+
+
+
+
